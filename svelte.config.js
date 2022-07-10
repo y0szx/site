384a3b9114304f63ac631a09b6271c428bc78785
y0szx <yuszx02@gmail.com>
2022-07-10 22:00:30 +0300

    eslint&prettier

--- svelte.config.js
+++ svelte.config.js
@@ -1,7 +1,7 @@
-import adapter from '@sveltejs/adapter-static';
-import preprocess from 'svelte-preprocess';
+import adapter from "@sveltejs/adapter-static";
+import preprocess from "svelte-preprocess";
 
-const dev = process.env.NODE_ENV === 'development';
+const dev = process.env.NODE_ENV === "development";
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
@@ -14,13 +14,13 @@ const config = {
 		},
 
 		paths: {
-			base: dev ? '' : '/site',
+			base: dev ? "" : "/site",
 		},
 
 		methodOverride: {
-			allowed: ['PATCH', 'DELETE']
-		}
-	}
+			allowed: ["PATCH", "DELETE"],
+		},
+	},
 };
 
 export default config;
